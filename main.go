71a06f9afb1e5f660415ac3c7eb148fe35548d99
Kai Hendry <kai.hendry@unee-t.com>
2018-06-27 15:17:28 +0800

    Fix https://github.com/unee-t/unit/issues/3

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	jsonhandler "github.com/apex/log/handlers/json"
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
-	"github.com/gorilla/pat"
+	"github.com/gorilla/mux"
 	"github.com/tj/go/http/response"
 	"github.com/unee-t/env"
 
@@ -30,8 +30,13 @@ type handler struct {
 	Code           env.EnvCode
 }
 
-type Unit struct {
-	ID string `json:"id"`
+type unit struct {
+	MefeUnitID             sql.NullString `json:"mefe_unit_id"`             // mefe_unit_id
+	MefeCreatorUserID      sql.NullString `json:"mefe_creator_user_id"`     // mefe_creator_user_id
+	BzfeCreatorUserID      int            `json:"bzfe_creator_user_id"`     // bzfe_creator_user_id
+	ClassificationID       int16          `json:"classification_id"`        // classification_id
+	UnitName               string         `json:"unit_name"`                // unit_name
+	UnitDescriptionDetails sql.NullString `json:"unit_description_details"` // unit_description_details
 }
 
 func init() {
@@ -40,6 +45,25 @@ func init() {
 	} else {
 		log.SetHandler(jsonhandler.Default)
 	}
+}
+
+func (h handler) step1Insert(unit unit) (err error) {
+	_, err = h.db.Exec(
+		`INSERT INTO ut_data_to_create_units (mefe_unit_id
+			mefe_creator_user_id,
+			bzfe_creator_user_id,
+			classification_id,
+			unit_name,
+			unit_description_details
+		) VALUES (?,?,?,?,?,?)`,
+		unit.MefeUnitID,
+		unit.MefeCreatorUserID,
+		unit.BzfeCreatorUserID,
+		unit.ClassificationID,
+		unit.UnitName,
+		unit.UnitDescriptionDetails,
+	)
+	return
 }
 
 // New setups the configuration assuming various parameters have been setup in the AWS account
@@ -97,12 +121,18 @@ func main() {
 	defer h.db.Close()
 
 	addr := ":" + os.Getenv("PORT")
-	app := pat.New()
-	app.Post("/create", env.Towr(env.Protect(http.HandlerFunc(h.createUnit), h.APIAccessToken)))
-	if err := http.ListenAndServe(addr, app); err != nil {
+	app := h.BasicEngine()
+
+	if err := http.ListenAndServe(addr, env.Protect(app, h.APIAccessToken)); err != nil {
 		log.WithError(err).Fatal("error listening")
 	}
 
+}
+
+func (h handler) BasicEngine() http.Handler {
+	app := mux.NewRouter()
+	app.HandleFunc("/create", h.createUnit).Methods("POST")
+	return app
 }
 
 func (h handler) runsql(sqlfile string, unitID string) (err error) {
@@ -120,10 +150,10 @@ func (h handler) createUnit(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("X-Robots-Tag", "none") // We don't want Google to index us
 
 	decoder := json.NewDecoder(r.Body)
-	var units []Unit
+	var units []unit
 	err := decoder.Decode(&units)
 	if err != nil {
-		log.WithError(err).Errorf("Input error")
+		log.WithError(err).Error("input error")
 		response.BadRequest(w, "Invalid JSON")
 		return
 	}
@@ -135,20 +165,27 @@ func (h handler) createUnit(w http.ResponseWriter, r *http.Request) {
 			"unit": unit,
 		})
 
-		ctx.Info("processing")
-
-		if unit.ID == "" {
-			ctx.Error("Missing ID")
+		if !unit.MefeUnitID.Valid {
+			ctx.Error("missing ID")
 			response.BadRequest(w, "Missing ID")
 			return
 		}
 
-		err = h.runsql("unit_create_new.sql", unit.ID)
+		err := h.step1Insert(unit)
+		if err != nil {
+			ctx.WithError(err).Error("failed to run step1Insert")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		ctx.Info("inserted")
+
+		err = h.runsql("unit_create_new.sql", unit.MefeUnitID.String)
 		if err != nil {
 			ctx.WithError(err).Errorf("unit_create_new.sql failed")
 			response.BadRequest(w, err.Error())
 			return
 		}
+		ctx.Info("ran unit_create_new.sql")
 	}
 
 	response.OK(w, units)
