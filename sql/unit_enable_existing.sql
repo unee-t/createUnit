f60438df83d29768b095b7947fd2b7cd11614911
franck.boullier - home <franck.boullier@gmail.com>
2018-06-29 09:16:19 +0800

    add script to enable a unit back

---
+++ sql/unit_enable_existing.sql
@@ -0,0 +1,34 @@
+# For any question about this script, ask Franck
+#
+# Pre-requisite:
+#	- The unit must already exists in the BZ table
+#
+# This script will
+#	- Enable the unit
+#	- Log what it does in the BZ database
+#	- Exit with no error if everything went as expected
+#
+#################################################################
+#
+# UPDATE THE BELOW VARIABLES ACCORDING TO YOUR NEEDS
+#
+#################################################################
+
+# We need the BZ product id for the unit
+	SET @product_id = '%s';
+	SET @environment = %d;
+
+# We also need to know the date when the unit was disabled in the MEFE
+	SET @active_when = NOW();
+
+#
+########################################################################
+#
+# ALL THE VARIABLES WE NEED HAVE BEEN DEFINED, WE CAN RUN THE SCRIPT
+#
+########################################################################
+
+# We have everything, we can now call the procedure which disables a unit
+	CALL `unit_enable_existing`;
+
+
