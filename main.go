365b7b5e1b3838f8552c1b2b04b1d4a3c8e87f44
franck <franck@unee-t.com>
2020-02-03 08:11:59 +0800

    debugging - move code from uneet/env directly here

--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	jsonhandler "github.com/apex/log/handlers/json"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/endpoints"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/tj/go/http/response"
-	"github.com/unee-t/env"
+
+	//	"github.com/unee-t/env"
 
 	"github.com/apex/log"
 	"github.com/apex/log/handlers/text"
@@ -31,7 +37,7 @@ type handler struct {
 	DSN            string // aurora database connection string
 	APIAccessToken string
 	db             *sql.DB
-	Code           env.EnvCode
+	Code           EnvCode
 }
 
 // We don't fine grain the types like sqlNullstring since that makes the JSON
@@ -49,6 +55,258 @@ type unitCreated struct {
 	ProductID int    `json:"id"`
 	UnitName  string `json:"name"`
 }
+
+// Debugging - This is the code that should be in uneet/env
+
+// Env is how we manage our differing {dev,demo,prod} AWS accounts
+
+type Env struct {
+	Code      EnvCode
+	Cfg       aws.Config
+	AccountID string
+	Stage     string
+}
+
+type EnvCode int
+
+// https://github.com/unee-t/processInvitations/blob/master/sql/1_process_one_invitation_all_scenario_v3.0.sql#L12-L16
+const (
+	EnvUnknown EnvCode = iota // Oops
+	EnvDev                    // Development aka Staging
+	EnvProd                   // Production
+	EnvDemo                   // Demo, which is like Production, for prospective customers to try
+)
+
+// GetSecret is the Golang equivalent for
+// aws --profile uneet-dev ssm get-parameters --names API_ACCESS_TOKEN --with-decryption --query Parameters[0].Value --output text
+
+func (e Env) GetSecret(key string) string {
+
+	val, ok := os.LookupEnv(key)
+	if ok {
+		log.Warnf("%s overridden by local env: %s", key, val)
+		return val
+	}
+	// Ideally environment above is set to avoid costly ssm (parameter store) lookups
+
+	ps := ssm.New(e.Cfg)
+	in := &ssm.GetParameterInput{
+		Name:           aws.String(key),
+		WithDecryption: aws.Bool(true),
+	}
+	req := ps.GetParameterRequest(in)
+	out, err := req.Send(context.TODO())
+	if err != nil {
+		log.WithError(err).Errorf("failed to retrieve credentials for looking up %s", key)
+		return ""
+	}
+	return aws.StringValue(out.Parameter.Value)
+}
+
+// NewConfig setups the configuration assuming various parameters have been setup in the AWS account
+func NewConfig(cfg aws.Config) (e Env, err error) {
+
+	defaultRegion, ok := os.LookupEnv("DEFAULT_REGION")
+	// the AWS variable `DEFAULT_REGION` is in the format `ap-southeast-1`
+	// We can use the repo https://github.com/aws/aws-sdk-go/ to convert this to a format like `ApSoutheast1RegionID`
+	// TODO - Check with @kai if the format `ap-southeast-1` is OK or if we need to transform that...
+	if ok {
+		log.Infof("DEFAULT_REGION overridden by local env: %s", defaultRegion)
+	} else {
+		defaultRegion = e.GetSecret("DEFAULT_REGION")
+	}
+
+	if defaultRegion == "" {
+		log.Fatal("DEFAULT_REGION is unset")
+	}
+
+	cfg.Region = defaultRegion
+	log.Warnf("Env Region: %s", cfg.Region)
+
+	// Save for ssm
+	e.Cfg = cfg
+
+	svc := sts.New(cfg)
+	input := &sts.GetCallerIdentityInput{}
+	req := svc.GetCallerIdentityRequest(input)
+	result, err := req.Send(context.TODO())
+	if err != nil {
+		return e, err
+	}
+
+	e.AccountID = aws.StringValue(result.Account)
+	log.Infof("Account ID: %s", result.Account)
+
+	e.Stage = e.GetSecret("STAGE")
+
+	switch e.Stage {
+	case "dev":
+		e.Code = EnvDev
+		return e, nil
+	case "prod":
+		e.Code = EnvProd
+		return e, nil
+	case "demo":
+		e.Code = EnvDemo
+		return e, nil
+	default:
+		log.WithField("stage", e.Stage).Error("unknown stage")
+		return e, nil
+	}
+}
+
+func (e Env) Bucket(svc string) string {
+	// Most common bucket
+	if svc == "" {
+		svc = "media"
+	}
+	installationID := e.GetSecret("INSTALLATION_ID")
+	if installationID == "" {
+		installationID = "main"
+		log.Warnf("Using fallback INSTALLATION_ID: %s: ", installationID)
+	}
+	if installationID == "main" {
+		// Preserve original bucket names
+		return fmt.Sprintf("%s-%s-unee-t", e.Stage, svc)
+	} else {
+		// Use INSTALLATION_ID to generate unique bucket name
+		return fmt.Sprintf("%s-%s-%s", e.Stage, svc, installationID)
+	}
+}
+
+func (e Env) SNS(name, region string) string {
+	if name == "" {
+		log.Warn("Service string empty")
+		return ""
+	}
+	return fmt.Sprintf("arn:aws:sns:%s:%s:%s", region, e.AccountID, name)
+}
+
+func (e Env) Udomain(service string) string {
+	if service == "" {
+		log.Warn("Service string empty")
+		return ""
+	}
+	domain := e.GetSecret("DOMAIN")
+	if domain == "" {
+		domain = "unee-t.com"
+		log.Warnf("Using fallback domain: %s: ", domain)
+	}
+	switch e.Code {
+	case EnvDev:
+		return fmt.Sprintf("%s.dev.%s", service, domain)
+	case EnvProd:
+		return fmt.Sprintf("%s.%s", service, domain)
+	case EnvDemo:
+		return fmt.Sprintf("%s.demo.%s", service, domain)
+	default:
+		log.Warnf("Udomain warning: Env %d is unknown, resorting to dev", e.Code)
+		return fmt.Sprintf("%s.dev.unee-t.com", service)
+	}
+}
+
+func (e Env) BugzillaDSN() string {
+	var bugzillaDbUser string
+	valbugzillaDbUser, ok := os.LookupEnv("BUGZILLA_DB_USER")
+	if ok {
+		log.Infof("BUGZILLA_DB_USER overridden by local env: %s", valbugzillaDbUser)
+		bugzillaDbUser = valbugzillaDbUser
+	} else {
+		bugzillaDbUser = e.GetSecret("BUGZILLA_DB_USER")
+	}
+
+	if bugzillaDbUser == "" {
+		log.Fatal("BUGZILLA_DB_USER is unset")
+	}
+
+	var bugzillaDbPassword string
+	valbugzillaDbPassword, ok := os.LookupEnv("BUGZILLA_DB_PASSWORD")
+	if ok {
+		log.Infof("BUGZILLA_DB_PASSWORD overridden by local env: %s", bugzillaDbPassword)
+		bugzillaDbPassword = valbugzillaDbPassword
+	} else {
+		bugzillaDbPassword = e.GetSecret("BUGZILLA_DB_PASSWORD")
+	}
+
+	if bugzillaDbPassword == "" {
+		log.Fatal("BUGZILLA_DB_PASSWORD is unset")
+	}
+
+	var mysqlhost string
+	valmysqlhost, ok := os.LookupEnv("MYSQL_HOST")
+	if ok {
+		log.Infof("MYSQL_HOST overridden by local env: %s", valmysqlhost)
+		mysqlhost = valmysqlhost
+	} else {
+		mysqlhost = e.GetSecret("MYSQL_HOST")
+	}
+
+	if mysqlhost == "" {
+		log.Fatal("MYSQL_HOST is unset")
+	}
+
+	var mysqlport string
+	valmysqlport, ok := os.LookupEnv("MYSQL_PORT")
+	if ok {
+		log.Infof("MYSQL_PORT overridden by local env: %s", valmysqlport)
+		mysqlport = valmysqlport
+	} else {
+		mysqlport = e.GetSecret("MYSQL_PORT")
+	}
+
+	if mysqlport == "" {
+		log.Fatal("MYSQL_PORT is unset")
+	}
+
+	var bugzillaDbName string
+	valbugzillaDbName, ok := os.LookupEnv("BUGZILLA_DB_NAME")
+	if ok {
+		log.Infof("BUGZILLA_DB_NAME overridden by local env: %s", valbugzillaDbName)
+		bugzillaDbName = valbugzillaDbName
+	} else {
+		bugzillaDbName = e.GetSecret("BUGZILLA_DB_NAME")
+	}
+
+	if bugzillaDbName == "" {
+		log.Fatal("BUGZILLA_DB_NAME is unset")
+	}
+
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?multiStatements=true&sql_mode=TRADITIONAL&timeout=15s&collation=utf8mb4_unicode_520_ci",
+		bugzillaDbUser,
+		bugzillaDbPassword,
+		mysqlhost,
+		mysqlport,
+		bugzillaDbName)
+}
+
+// Protect using: curl -H 'Authorization: Bearer secret' style
+// Modelled after https://github.com/apex/up-examples/blob/master/oss/golang-basic-auth/main.go#L16
+func Protect(h http.Handler, APIAccessToken string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var token string
+		// Get token from the Authorization header
+		// format: Authorization: Bearer
+		tokens, ok := r.Header["Authorization"]
+		if ok && len(tokens) >= 1 {
+			token = tokens[0]
+			token = strings.TrimPrefix(token, "Bearer ")
+		}
+		if token == "" || token != APIAccessToken {
+			log.Errorf("Token %q != APIAccessToken %q", token, APIAccessToken)
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+// Towr is a workaround for gorilla/pat: https://stackoverflow.com/questions/50753049/
+// Wish I could make this simpler
+func Towr(h http.Handler) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) { h.ServeHTTP(w, r) }
+}
+
+// END Debugging
 
 func init() {
 	if os.Getenv("UP_STAGE") == "" {
@@ -86,7 +344,7 @@ func New() (h handler, err error) {
 		return
 	}
 	cfg.Region = endpoints.ApSoutheast1RegionID
-	e, err := env.New(cfg)
+	e, err := NewConfig(cfg)
 	if err != nil {
 		log.WithError(err).Warn("error getting unee-t env")
 	}
@@ -159,7 +417,7 @@ func (h handler) BasicEngine() http.Handler {
 		// local dev, get around permissions
 		return app
 	}
-	return env.Protect(app, h.APIAccessToken)
+	return Protect(app, h.APIAccessToken)
 }
 
 func (h handler) runsql(sqlfile string, unitID string) (res sql.Result, err error) {
