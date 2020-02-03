9112dc1df0fec195d7d9edd9830d958fb062f118
franck <franck@unee-t.com>
2020-02-03 10:55:54 +0800

    increase timeout to get more time to get AWS parameters

--- main.go
+++ main.go
@@ -268,7 +268,7 @@ func (e Env) BugzillaDSN() string {
 		log.Fatal("BUGZILLA_DB_NAME is unset")
 	}
 
-	return fmt.Sprintf("%s:%s@tcp(%s:3306)/bugzilla?multiStatements=true&sql_mode=TRADITIONAL&timeout=5s&collation=utf8mb4_unicode_520_ci",
+	return fmt.Sprintf("%s:%s@tcp(%s:3306)/bugzilla?multiStatements=true&sql_mode=TRADITIONAL&timeout=15s&collation=utf8mb4_unicode_520_ci",
 		bugzillaDbUser,
 		bugzillaDbPassword,
 		mysqlhost)
